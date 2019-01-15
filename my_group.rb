b2ea1a2913e26c1c72a08dae70d0cc5cef851b83
Alecia <aleciabenjamin@gmail.com>
2019-01-15 19:24:18 +0100

    Creates README file with user stories

---
+++ my_group.rb
@@ -0,0 +1,4 @@
+my_group = [person_1, person_2, person_3]
+person_1 = {name: "Liesl", gender: "female", age: 40}
+person_2 = {name: "Caroline", gender: "female", age: 65}
+person_3 = {name: "Henry", gender: "male", age: 2}
